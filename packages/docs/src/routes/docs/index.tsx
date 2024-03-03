c8a32bfd08d1443e91bc2b52ec040d388ef5bd90
saboooor <saboor547@gmail.com>
2024-03-03 15:34:25 -0700

    add readme to website

--- packages/docs/src/routes/docs/index.tsx
+++ packages/docs/src/routes/docs/index.tsx
@@ -4,7 +4,7 @@ import type { DocumentHead } from '@builder.io/qwik-city';
 import Anchor from '~/components/docs/Anchor';
 import Blobs from '~/components/docs/Blobs';
 import Button from '~/components/docs/Button';
-import Card from '~/components/docs/Card';
+import CardSection from '~/components/docs/Card';
 import ColorInput from '~/components/docs/ColorInput';
 import Nav from '~/components/docs/Nav';
 import NumberInput from '~/components/docs/NumberInput';
@@ -13,17 +13,30 @@ import TextArea from '~/components/docs/TextArea';
 import TextInput from '~/components/docs/TextInput';
 import Toggle from '~/components/docs/Toggle';
 import IconsLogos from '~/components/docs/IconsLogos';
+import { Card, Header } from '@luminescent/ui';
+import Readme from '../../../../../README.md';
 
 export default component$(() => {
   return (
     <section class="max-w-6xl mx-auto py-32 px-4 flex flex-col gap-4">
-      <div class="text-4xl font-extrabold tracking-wide mb-2 flex items-center gap-4">
+      <h1 class="text-4xl font-extrabold tracking-wide mb-2 flex items-center gap-4">
+        Luminescent UI Documentation
+      </h1>
+      <Header>
+        Get Started
+      </Header>
+      <Card>
+        <div class="flex flex-col gap-4 text-sm [&>pre]:p-4 [&>pre]:bg-gray-950/40 [&>pre]:border [&>pre]:border-gray-800 [&>pre]:rounded-lg [&>pre]:text-gray-400">
+          <Readme />
+        </div>
+      </Card>
+      <Header>
         Elements
-      </div>
+      </Header>
       <Anchor />
       <Blobs />
       <Button />
-      <Card />
+      <CardSection />
       <ColorInput />
       <Nav />
       <NumberInput />
