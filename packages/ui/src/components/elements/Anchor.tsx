ac9cd9acc1b1e966c9303026778fca1414cd7c29
saboooor <saboor547@gmail.com>
2024-03-11 15:00:45 -0600

    idk

--- packages/ui/src/components/elements/Anchor.tsx
+++ packages/ui/src/components/elements/Anchor.tsx
@@ -3,4 +3,4 @@ import { component$ } from '@builder.io/qwik';
 
 interface AnchorProps { id: string; }
 
-export const Anchor = component$<AnchorProps>(({ id }) => <span id={id} class="block h-32 -mt-32" />);
+export const Anchor = component$<AnchorProps>(({ id }) => <span id={id} class="block h-32 -mt-32 pointer-events-none" />);
