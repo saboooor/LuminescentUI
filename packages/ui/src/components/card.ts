1d0f948db296cf871985f924dbf20234d541134b
saboooor <saboor547@gmail.com>
2024-08-21 05:52:50 -0600

    uh

--- packages/ui/src/components/card.ts
+++ packages/ui/src/components/card.ts
@@ -5,7 +5,7 @@ export const defaultClasses = 'lum-bg-gray-900 lum-pad-equal-4xl rounded-lg';
 export default function ({ addComponents }: PluginAPI) {
   addComponents({
     '.lum-card': {
-      '@apply flex flex-col gap-3 whitespace-nowrap': {},
+      '@apply flex flex-col gap-3 whitespace-pre-wrap': {},
       [`@apply ${defaultClasses}`]: {},
     },
   });
